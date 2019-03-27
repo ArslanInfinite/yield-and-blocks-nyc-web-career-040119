b825b0daadd8990db49bb4ecfbe19da8d3e18601
Arslan Ali <ali.arslan319@gmail.com>
2019-03-27 23:27:25 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,19 @@
 def hello_t(names_array)
     names_array.each do |name|
       if name.start_with?("T", "J")
-        puts "Hi, #{name}"
+        puts "#{name}"
       end
     end
 end
 
 # call your method here!
+
+
+def reverse_each_word(sentence1)
+  new_sentence = []
+  new_sentence = sentence1.split(" ")
+      brand_new_sentence = new_sentence.collect do |word|
+          word.reverse
+        end
+        brand_new_sentence.join(" ")
+end
