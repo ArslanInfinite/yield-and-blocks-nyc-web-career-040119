5bab63af13c34f9653830f0e79e1790bf3771903
Arslan Ali <ali.arslan319@gmail.com>
2019-03-27 23:28:17 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,14 +6,14 @@ def hello_t(names_array)
     end
 end
 
-# call your method here!
+hello_t(names_array)
 
 
-def reverse_each_word(sentence1)
-  new_sentence = []
-  new_sentence = sentence1.split(" ")
-      brand_new_sentence = new_sentence.collect do |word|
-          word.reverse
-        end
-        brand_new_sentence.join(" ")
-end
+# def reverse_each_word(sentence1)
+#   new_sentence = []
+#   new_sentence = sentence1.split(" ")
+#       brand_new_sentence = new_sentence.collect do |word|
+#           word.reverse
+#         end
+#         brand_new_sentence.join(" ")
+# end
