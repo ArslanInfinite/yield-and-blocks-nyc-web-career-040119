6abddb72c6aa0ca0dffaa96f95036c2f06b636be
Arslan Ali <ali.arslan319@gmail.com>
2019-03-27 23:33:15 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,29 @@
-def hello_t
-
+def hello_t(names_array)
+    names_array.each do |name|
+      if name.start_with?("T", "J")
+        puts "#{name}"
+      end
+    end
 end
 
-# call your method here!
+def hello_t(names_array)
+  if block_given?
+    i = 0
+     while i < names_array.length
+      yield(names_array[i])
+      i = i + 1
+    end
+    names_array
+  else
+    puts "Hey! No block was given!"
+  end
+end
 
+# def reverse_each_word(sentence1)
+#   new_sentence = []
+#   new_sentence = sentence1.split(" ")
+#       brand_new_sentence = new_sentence.collect do |word|
+#           word.reverse
+#         end
+#         brand_new_sentence.join(" ")
+# end
